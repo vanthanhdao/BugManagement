ea75abe1cf26993a7f51976fa1d3abe1e2ad7b34
vanthanhdao <daothanh1411@gmail.com>
2024-01-18 20:24:58 +0700

    18/1/24

---
+++ QuanlyBug/Models/ResetPassword.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace QuanlyBug.Models
+{
+    public class ResetPassword
+    {
+        [Required(ErrorMessage = "Bắt buộc phải nhập password", AllowEmptyStrings = false)]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "Mật khẩu không trùng khớp")]
+        public string ConfirmPassword { get; set; }
+        [Required]
+        public string ResetCode { get; set; }
+    }
+}
